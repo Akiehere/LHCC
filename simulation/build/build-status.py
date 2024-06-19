bcaf20a48f2be95fb0e9f718d5b6c8813140a453
dongbo <bodong.yan@qq.com>
2024-06-19 17:04:24 +0800

    update

--- simulation/build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.18-scratch-simulator-debug', 'build/scratch/ns3.18-third-debug', 'build/scratch/subdir/ns3.18-subdir-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
